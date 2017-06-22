acf66da9a8a17cae98120e246cbeb59f4caf8f82
Curry Lee <ej20curry@gmail.com>
2017-06-22 04:55:10 -0400

    woowwwww this is stupid

--- js/app.js
+++ js/app.js
@@ -8,25 +8,71 @@
   .controller("PostIndexController", [
     PostIndexControllerFunction
   ])
+  angular
+  .module("wdinstagram", [
+    "ui.router",
+    "ngResource"
+  ])
   .controller("PostShowController", [
     "$stateParams",
     PostShowControllerFunction
   ])
+  .controller("PostIndexController", [
+    "PostFactory",
+    PostIndexControllerFunction
+  ]);
+
+  .controller("PostShowController", [
+    "PostFactory",
+    "$stateParams",
+    PostShowControllerFunction
+  ])
+  .controller("PostNewController", [
+    "PostFactory",
+    PostNewControllerFunction
+  ])
+
+  .controller("PostEditController", [
+    "PostFactory",
+    "$stateParams",
+    PostEditControllerFunction
+  ])
 
 
-function PostIndexControllerFunction(){
-  this.posts = [
-    {author: "No one", body: "Send it!", photo_url: "https://video-images.vice.com/_uncategorized/1487797275177-Larry-Enticer.png"},
-    {author: "Your Parents", body: "Once upon a time... the end", photo_url: "http://i.imgur.com/yErFr19.jpg"}
+  .factory( "PostFactory", [
+        "$resource",
+        PostFactoryFunction
+      ]);
 
-  ]
+      function postFactoryFunction( $resource ){
+        return $resource( "http://localhost:3000/posts/:id", {}, {
+          update: { method: "PUT" }
+        });
+      }
+
+
+function PostIndexControllerFunction(PostFactory){
+  this.posts = PostFactory.query();
 }
 
-function PostShowControllerFunction ($stateParams) {
+function PostShowControllerFunction (PostFactory, $stateParams) {
   console.log($stateParams); //for debug purposes and whatnot
 this.insta = posts[$stateParams.id];
 }
 
+function PostNewControllerFunction (PostFactory) {
+  console.log($stateParams); //for debug purposes and whatnot
+this.insta = new PostFactory();
+this.create = function(){
+  this.insta.$save()
+  }
+}
+function PostEditControllerFunction(PostFactory, $stateParams){
+  this.insta = PostFactory.get({id: $stateParams.id});
+  this.update = function(){
+    this.insta.$update({id: $stateParams.id})
+  }
+}
 
 
 function Router($stateProvider){
@@ -55,4 +101,16 @@ $stateProvider
     controller: "PostIndexController",
     controllerAs: "vm"
 
+  })
+  .state("postNew", {
+    url: "posts/new",
+    templateUrl: "js/ng-views/new.html",
+    controller: "PostNewController",
+    controllerAs: "vm"
+  })
+  .state("postShow", {
+    url: "posts/show",
+    templateUrl: "js/ng-views/show.html",
+    controller: "PostShowController",
+    controllerAs: "vm"
   });
